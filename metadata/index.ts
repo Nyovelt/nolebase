d832508189b3079574d1ef867207b57c90ac153d
nyovelt <canarypwn@aosc.io>
2024-03-06 19:54:27 -0500

    Update index.ts

--- metadata/index.ts
+++ metadata/index.ts
@@ -4,13 +4,13 @@ export const siteShortName = 'Nólëbase'
 export const siteDescription = '记录回忆，知识和畅想的地方'
 
 /** 文档所在目录 */
-export const include = ['笔记', '生活']
+export const include = ['Notes']
 
 /** Repo */
 export const githubRepoLink = 'https://github.com/nolebase/nolebase'
 
 /** 无协议前缀域名 */
-export const plainTargetDomain = 'nolebase.ayaka.io'
+export const plainTargetDomain = 'nolebase.aaaab3n.moe'
 export const targetDomain = `https://${plainTargetDomain}`
 
 /** 社交媒体 */
